4a70bea56f3dfa68b00db3183559162d59c7d1b5
amina <amina.1111@yahoo.fr>
2025-02-23 21:06:32 +0100

    sandpiles

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,103 @@
+#include "sandpiles.h"
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ */
+void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * is_unstable - Checks if the grid is unstable
+ * @grid: 3x3 grid
+ * Return: 1 if unstable, 0 if stable
+ */
+int is_unstable(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * topple_grid - Topples the grid
+ * @grid: 3x3 grid
+ */
+void topple_grid(int grid[3][3])
+{
+	int i, j;
+	int temp[3][3] = {0};
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				temp[i][j] -= 4;
+				if (i > 0)
+					temp[i - 1][j] += 1;
+				if (i < 2)
+					temp[i + 1][j] += 1;
+				if (j > 0)
+					temp[i][j - 1] += 1;
+				if (j < 2)
+					temp[i][j + 1] += 1;
+			}
+		}
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] += temp[i][j];
+		}
+	}
+}
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: first grid
+ * @grid2: second grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+
+	/* Topple the grid until it's stable */
+	while (is_unstable(grid1))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		topple_grid(grid1);
+	}
+}
